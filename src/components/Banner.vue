28008054514b787e52581711f9575bfd94bd3ee3
turan-98 <santanasamuel08@gmail.com>
2024-03-05 18:46:14 -0300

    ⚡️

---
+++ src/components/Banner.vue
@@ -0,0 +1,72 @@
+<template>
+    <!-- single file component  tudo dentro de um único arquivo CSS, HTML e JS-->
+    <header class="banner">
+    <div class="presentation">
+      <img src="../assets/img/logo.svg" class="logo" alt="Logo do Cooking">
+      <p class="header-phrase-header">
+        <span class="green-text">Um banquete de ideias para</span>
+        despertar o chef
+      </p>
+
+      <p class="subtitle-lg">Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance</p>
+
+    </div>
+    <img src="../assets/img/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="banner-photo">
+  </header>
+</template>
+<!-- Estilo que afeta somente o componente em que está, isso resolve conflitos de CSS
+O vue adiciona um identificador único para cada style do component, para ter um escopo global, basta retirar a tag scoped, é possível utilizar o scoped e global juntos
+-->
+<style scoped>
+.banner {
+  padding: 4rem 7.5rem;
+  color: var(--creme);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  column-gap: 3.25rem;
+}
+
+.logo {
+  height: 4.5rem;
+  margin-bottom: 3rem;
+}
+
+.header-phrase-header{
+  margin-bottom: 2rem;
+}
+
+.green-text {
+  color: var(--verde-medio, #3D6D4A);
+}
+
+.banner-photo {
+  width: 35rem;
+}
+
+@media only screen and (max-width: 1300px) {
+  .banner {
+    padding: 4rem 3.75rem;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+  }
+
+  .logo {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .banner {
+    padding: 4rem 1.5rem;
+  }
+
+  .banner-photo {
+    width: min(100%, 21.25rem);
+  }
+}
+</style>
